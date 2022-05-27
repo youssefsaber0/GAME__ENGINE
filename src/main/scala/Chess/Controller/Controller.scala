daab3a974a566805fd62bcaff879020cfeb667d1
Meniem Hany <es-abdelmoneim.hany2019@alexu.edu.eg>
2022-05-27 14:54:03 +0200

    Begin v3

--- src/main/scala/Chess/Controller/Controller.scala
+++ src/main/scala/Chess/Controller/Controller.scala
@@ -2,4 +2,33 @@ package Chess.Controller
 
 class Controller {
 
+  def initializeBoard() = {
+    var board = Array(
+      Array('R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'),
+      Array('P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'),
+      Array('-', '-', '-', '-', '-', '-', '-', '-'),
+      Array('-', '-', '-', '-', '-', '-', '-', '-'),
+      Array('-', '-', '-', '-', '-', '-', '-', '-'),
+      Array('-', '-', '-', '-', '-', '-', '-', '-'),
+      Array('p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'),
+      Array('r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'),
+    )
+    for (i <- 0 to 7) {
+      for(j <- 0 to 7){
+        print(board(i)(j) + " ")
+      }
+      println()
+    }
+    board
+  }
+
+
+
+}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val cont = new Controller
+    cont.initializeBoard()
+  }
 }
